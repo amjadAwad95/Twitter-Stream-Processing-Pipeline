07da5d78d96f06a9d9fad88554e38921f3f8e393
YahyaDalbah <yahya1dalbah@gmail.com>
2024-12-20 21:13:45 +0200

    data porcessing done without elasticsearch

---
+++ Data-Processing/src/main/scala/DataProcessing.scala
@@ -0,0 +1,64 @@
+import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.varia.NullAppender
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+
+object DataProcessing {
+  def dataProcessing(): Unit = {
+    val nullAppender = new NullAppender
+    BasicConfigurator.configure(nullAppender)
+    val spark = SparkSession.builder()
+      .appName("dataProcessor")
+      .master("local") // cluster settings (fun info)
+      .getOrCreate()
+
+    val tweetSchema = new StructType()
+      .add("id", StringType)
+      .add("text", StringType)
+      .add("entities", new StructType().add("hashtags", ArrayType(new StructType().add("text", StringType))))
+      .add("created_at", StringType)
+      .add("geo", new StructType().add("coordinates", ArrayType(DoubleType)))
+      .add("user", new StructType()
+        .add("id", StringType)
+        .add("name", StringType)
+        .add("screen_name", StringType)
+        .add("location", StringType))
+    import spark.implicits._
+    val kafkaDf = KafkaConsumer.startKafkaConsumer()
+    val rawDF = kafkaDf.select("tweets")
+      .select(from_json($"tweets", tweetSchema).as("tweets"))
+      .select("tweets.*")
+
+
+
+    val transformedDF = rawDF
+      .withColumn("hashtags", expr("transform(entities.hashtags, h -> h.text)")) // Extract hashtag texts
+      .withColumn("geo_coordinates", struct(
+        $"geo.coordinates"(0).alias("lat"),
+        $"geo.coordinates"(1).alias("lon")
+      ))
+      .withColumn("user", struct(
+        $"user.id",
+        $"user.name",
+        $"user.screen_name".alias("username"),
+        $"user.location"
+      ))
+      .select(
+        $"id".alias("tweet_id"),
+        $"text",
+        $"hashtags",
+        $"created_at",
+        $"geo_coordinates",
+        $"user"
+      )
+  val query = transformedDF.writeStream
+    .outputMode("append") // Use "append" or "update" based on requirements
+    .format("json") // Output format
+    .option("path", "output/json") // Directory where JSON files will be written
+    .option("checkpointLocation", "output/checkpoint") // Directory for storing metadata (gives error if not there)
+    .start()
+    query.awaitTermination()
+  }
+}
